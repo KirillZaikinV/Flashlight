d1a8c94ae511e14f0898dac22577d1fd8d5fce2f
Kirilll Zaikin <Kirill@macbook-pro-kirilll.local>
2019-02-02 08:41:14 +0100

    Initial Commit

---
+++ GettingStarted/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GettingStarted
+//
+//  Created by Kirilll Zaikin on 02/02/2019.
+//  Copyright © 2019 Kirilll Zaikin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
